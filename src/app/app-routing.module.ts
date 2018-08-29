314bf46c6411932a5f0d834786a49ccdcffb2ef0
Ngela <ngela@Ngela-Pat-Yu.home>
2018-08-29 17:13:34 +0200

    app/*-Import needed modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'music',
+    loadChildren: './modules/music/music.module#MusicModule'
+  },
+  {
+    path: '',
+    redirectTo: '/music',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared';
+import { CoreModule } from './core'
 @NgModule({
   declarations: [
     AppComponent
@@ -12,8 +13,10 @@ import { SharedModule } from './shared';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
+    CoreModule,
+    SharedModule,
     AppRoutingModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
